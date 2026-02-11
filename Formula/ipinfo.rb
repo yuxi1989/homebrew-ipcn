ede264960eb6b4515a33b175789582988f89a257
zhuxiaoyu <zxy@ip669.com>
2026-02-11 21:26:04 +0800

    Update ipinfo formula checksum for v0.1.0

--- Formula/ipinfo.rb
+++ Formula/ipinfo.rb
@@ -2,7 +2,7 @@ class Ipinfo < Formula
   desc "IP/Domain geolocation & ASN lookup CLI"
   homepage "https://github.com/yuxi1989/ipinfo"
   url "https://github.com/yuxi1989/ipinfo/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "60a77ae24b1c269ed8b6d3d52766de62ff7a6b60518cdc2da75130d16cb02261"
   license "MIT"
 
   depends_on "jq"
